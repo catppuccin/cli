1da767058233419a26d5554dce628a946eff02a5
Ace Daksh <ektadaksh7@gmail.com>
2022-08-14 15:48:56 -0700

    feat: read .ctprc

--- uwu.go
+++ uwu.go
@@ -3,8 +3,33 @@ package main
 import (
 	"fmt"
 	"os"
+	"io"
+	"os/exec"
+//  "gopkg.in/yaml.v3"
 	"net/http"
 )
+
+type Program struct {
+	app_name string
+	path_name string
+	install_location string
+	install_files []string
+	install_flavours struct {
+		latte []struct {
+			preference string
+		}
+		frappe []struct {
+			preference string
+		}
+		macchiato []struct {
+			preference string
+		}
+		mocha []struct {
+			preference string
+		}
+	}
+	one_flavour bool
+}
 
 func main() {
 	packages := os.Args[1:]
@@ -13,28 +38,51 @@ func main() {
 	for i := 0; i < len(packages); i++ {
 		fmt.Println(packages[i])
 	}
-	fmt.Println("Generating chezmoi config...")
+	fmt.Println("\nGenerating chezmoi config...\n")
 	success := []string{}
 	for i := 0; i < len(packages); i++ {
 		repo := packages[i]
 		// Attempt to get the .catppuccinrc
-		rc := "https://raw.githubusercontent.com/catppuccin/" + repo + "/main/.catppuccinrc"
+		rc := "https://raw.githubusercontent.com/catppuccin/" + repo + "/main/.ctprc"
 		res, err := http.Get(rc)
 		if err != nil {
-			fmt.Printf("Failed to make HTTP request: %s\n", err)
+			fmt.Printf("\nFailed to make HTTP request: %s\n", err)
 			os.Exit(1)
 		}
 		if res.StatusCode != 200 {
-			fmt.Printf("%s does not have a .catppuccinrc.\n", repo)
-			break
+			fmt.Printf("%s does not have a .ctprc.\n", repo)
+			continue
+		} else { 
+			success = append(success, repo)
 		}
-		success = append(success, repo)
 	}
-	fmt.Println(success)
+	fmt.Println("\nChecking for installed packages...\n")
+	for i := 0; i < len(packages); i++ {
+		path, err := exec.LookPath(packages[i])
+		if err != nil {
+			// Program is not installed/could not be detected
+			fmt.Printf("%s was not detected.\n", packages[i])
+			packages = remove_at(packages, i)
+		}
+		fmt.Printf("%s found at location %s.\n", packages[i], path)
+		rc := "https://raw.githubusercontent.com/catppuccin/" + packages[i] + "/main/.ctprc"
+		res, err := http.Get(rc)
+		defer res.Body.Close()
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("Failed to read body.")
+		} else {
+			fmt.Printf(".ctprc - \n%s", body)
+		}
+	}
 }
 
 func gen_chezmoi(repo string, dir string, refresh int) string {
 	// Creates a chezmoi entry using the repo name, updates every week
 	res := fmt.Sprintf("%s:\n  type: git-repo\n  url: \"%s.git\"\n  refreshPeriod: %dh\n", dir, repo, refresh)
 	return res
+}
+
+func remove_at(list []string, index int) []string {
+	return append(list[:index], list[index+1:]...)
 }
