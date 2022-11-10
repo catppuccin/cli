b1db51ef79d32916329f4c4e30218734ecc9c958
jolheiser <john.olheiser@gmail.com>
2022-11-09 23:08:41 -0600

    chore: use is for assertions in all tests
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -3,32 +3,25 @@ package utils
 import (
 	"os"
 	"testing"
+
+	"github.com/matryer/is"
 )
 
 func TestHandleDir(t *testing.T) {
+	assert := is.New(t)
+
 	os.Setenv("CATPPUCCIN", "pog")
 	os.Setenv("NEOVIM", "chad/")
+
 	r1 := HandleDir("this/shouldnt/change")
+	assert.True(r1 == "this/shouldnt/change") // Unchanging directory should not change
+
 	r2 := HandleDir("$CATPPUCCIN/is/pog")
+	assert.True(r2 == "pog/is/pog") // Catppuccin should always be pog
+
 	r3 := HandleDir("catppuccin/is/$CATPPUCCIN/nice")
+	assert.True(r3 == "catppuccin/is/pog/nice") // Catppuccin should always be pog, even in the middle
+
 	r4 := HandleDir("neovim/is/a/$NEOVIM/text/editor")
-	if r1 != "this/shouldnt/change" {
-		t.Log("Unchanging directory changed.")
-		t.Fail()
-	}
-	if r2 != "pog/is/pog" {
-		t.Log("Catppuccin wasn't pog, which is obviously untrue.")
-		t.Log(r2)
-		t.Fail()
-	}
-	if r3 != "catppuccin/is/pog/nice" {
-		t.Log("Catppuccin in the middle? Still pog.")
-		t.Log(r3)
-		t.Fail()
-	}
-	if r4 != "neovim/is/a/chad/text/editor" {
-		t.Log("Catppuccin in the middle? Still pog.")
-		t.Log(r4)
-		t.Fail()
-	}
+	assert.True(r4 == "neovim/is/a/chad/text/editor") // Neovim should always be a chad editor
 }
